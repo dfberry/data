7bd0070db3097ec05b543a796931aaa0647a4f59
Dina Berry <dinaberry@outlook.com>
2024-04-27 15:51:59 +0000

    daily issues

--- automation/issues/dist/index.js
+++ automation/issues/dist/index.js
@@ -10,6 +10,14 @@ async function loadRepos() {
     console.log(repos);
     return repos;
 }
+async function loadUsers() {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, '../users.json');
+    const data = await readFile(filePath, 'utf8');
+    const users = JSON.parse(data);
+    console.log(users);
+    return users;
+}
 function convertIssueToResult(issue) {
     return {
         date_created: issue.created_at,
@@ -21,24 +29,34 @@ function convertIssueToResult(issue) {
         url: issue.url
     };
 }
-async function fetchData(url, repo) {
+async function fetchRepoData(url) {
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
-    // @ts-ignore
-    const data = await response.json();
+    const data = (await response.json());
     if (data.length === 0) {
         return [];
     }
     return data;
 }
-async function processRepos(beginDate, endDate) {
-    let results = [];
+async function fetchIssues(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = (await response.json());
+    if (data.total_count === 0) {
+        return [];
+    }
+    return data.items;
+}
+async function processRepos(beginDate, endDate, repos) {
+    const results = [];
     // Get data
     for await (const repo of repos) {
         const url = `https://api.github.com/repos/${repo}/issues?created:${beginDate}..${endDate}`;
-        const repoResult = await fetchData(url, repo);
+        const repoResult = await fetchRepoData(url);
         // Transform data
         for (const item of repoResult) {
             results.push(convertIssueToResult(item));
@@ -46,42 +64,105 @@ async function processRepos(beginDate, endDate) {
     }
     return results;
 }
+async function processUserIssues(beginDate, endDate, user) {
+    const results = [];
+    const arrayUrls = [];
+    arrayUrls.push(`https://api.github.com/search/issues?q=assignee:${user}+created:${beginDate}..${endDate}`);
+    arrayUrls.push(`https://api.github.com/search/issues?q=mentions:${user}+created:${beginDate}..${endDate}`);
+    for await (const url of arrayUrls) {
+        const userIssues = await fetchIssues(url);
+        // Transform data
+        for (const item of userIssues) {
+            results.push(convertIssueToResult(item));
+        }
+    }
+    return results;
+}
 function getDates(beginDays, endDays) {
+    // Parse beginDays and endDays as integers
+    const beginDaysInt = parseInt(beginDays, 10);
+    const endDaysInt = parseInt(endDays, 10);
+    // Check if beginDays and endDays are valid integers
+    if (isNaN(beginDaysInt) || isNaN(endDaysInt)) {
+        throw new Error('beginDays and endDays must be valid integers');
+    }
     // Get the current date
-    let currentDate = new Date();
-    // Get the days of 2 days ago
+    const currentDate = new Date();
+    // Get the date of beginDays days ago
     const beginDate = new Date();
     beginDate.setDate(currentDate.getDate() - parseInt(beginDays));
-    // Get the date of 1 day ago
+    // Get the date of endDays days ago
     const endDate = new Date();
-    endDate.setDate(currentDate.getDate() - parseFloat(endDays));
+    endDate.setDate(currentDate.getDate() - parseInt(endDays));
     // Format the dates in YYYY-MM-DD format
     const beginDateString = beginDate.toISOString().split('T')[0];
     const endDateString = endDate.toISOString().split('T')[0];
     // Output as markdown with links to URLs
-    const formatBeginDate = `${beginDate.toLocaleString('default', { month: 'long' })} ${beginDate.getDate()}, ${beginDate.getFullYear()}`;
-    const formatEndDate = `${endDate.toLocaleString('default', { month: 'long' })} ${endDate.getDate()}, ${endDate.getFullYear()}`;
-    return { beginDate, endDate, formatBeginDate, formatEndDate, beginDateString, endDateString };
+    const formatBeginDate = `${beginDate.toLocaleString('default', {
+        month: 'long'
+    })} ${beginDate.getDate()}, ${beginDate.getFullYear()}`;
+    const formatEndDate = `${endDate.toLocaleString('default', {
+        month: 'long'
+    })} ${endDate.getDate()}, ${endDate.getFullYear()}`;
+    return {
+        beginDate,
+        endDate,
+        formatBeginDate,
+        formatEndDate,
+        beginDateString,
+        endDateString
+    };
+}
+function printData(beginDate, endDate, reportName, data) {
+    console.log(`# ${reportName} from ${beginDate} to ${endDate}`);
+    console.log(`|Id|Type|Repo|User|Title|Date|`);
+    console.log(`|--|--|--|--|--|--|`);
+    data.forEach((item) => {
+        // split url into its parts
+        const urlParts = item.url.split('/');
+        const owner = urlParts[3];
+        const repo = urlParts[4];
+        const user = item.user;
+        // remove time from date
+        item.date_created = item.date_created.split(',')[0];
+        console.log(`|${item.id}|${item.type}|${owner}/${repo}|${user}| [${item.title}](${item.url})|${item.date_created}|`);
+    });
 }
 // Load list of repos
 const repos = await loadRepos();
+// Load list of users
+const users = await loadUsers();
 // Get the dates
-const previousDaysBegin = process.env.PREVIOUS_DAYS_BEGIN;
-const previousDaysEnd = process.env.PREVIOUS_DAYS_End;
+const previousDaysBegin = process.env.PREVIOUS_DAYS_BEGIN || '8';
+const previousDaysEnd = process.env.PREVIOUS_DAYS_END || '1';
 const dates = getDates(previousDaysBegin, previousDaysEnd);
 // Get data and transform it
-const issuesAndPrs = await processRepos(dates.beginDateString, dates.endDateString);
-// Present data
+const issuesAndPrs = await processRepos(dates.beginDateString, dates.endDateString, repos);
+printData(dates.formatBeginDate, dates.formatEndDate, 'Repos - GitHub issues and prs', issuesAndPrs);
+// Assigned to users
+const assignedIssuesAndPrs = [];
+// Get data for users
+for await (const user of users) {
+    const userIssues = await processUserIssues(dates.beginDateString, dates.endDateString, user);
+    assignedIssuesAndPrs.push(...userIssues);
+}
+printData(dates.formatBeginDate, dates.formatEndDate, 'Assigned - GitHub issues and prs', assignedIssuesAndPrs);
+/* // Present data
 console.log(`# GitHub issues and prs from ${dates.formatBeginDate} to ${dates.formatEndDate}`);
 console.log(`|Id|Type|Repo|User|Title|Date|`);
 console.log(`|--|--|--|--|--|--|`);
-issuesAndPrs.forEach((item) => {
+
+issuesAndPrs.forEach((item: Result) => {
+
     // split url into its parts
     const urlParts = item.url.split('/');
     const owner = urlParts[3];
     const repo = urlParts[4];
     const user = item.user;
+
     // remove time from date
     item.date_created = item.date_created.split(',')[0];
-    console.log(`|${item.id}|${item.type}|${owner}/${repo}|${user}| [${item.title}](${item.url})|${item.date_created}|`);
-});
+
+    console.log(`|${item.id}|${item.type}|${owner}/${repo}|${user}| [${item.title}](${item.url})|${item.date_created}|`)
+}); */
+//# sourceMappingURL=index.js.map
--- automation/issues/src/index.ts
+++ automation/issues/src/index.ts
@@ -2,194 +2,174 @@ import fetch from 'node-fetch';
 import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-
-type Issue = {
-    url: string;
-    repository_url: string;
-    labels_url: string;
-    comments_url: string;
-    events_url: string;
-    html_url: string;
-    id: number;
-    node_id: string;
-    number: number;
-    title: string;
-    user: {
-        login: string;
-        id: number;
-        node_id: string;
-        avatar_url: string;
-        gravatar_id: string;
-        url: string;
-        html_url: string;
-        followers_url: string;
-        following_url: string;
-        gists_url: string;
-        starred_url: string;
-        subscriptions_url: string;
-        organizations_url: string;
-        repos_url: string;
-        events_url: string;
-        received_events_url: string;
-        type: string;
-        site_admin: boolean;
-    };
-    labels: {
-        id: number;
-        node_id: string;
-        url: string;
-        name: string;
-        color: string;
-        default: boolean;
-        description: string | null;
-    }[];
-    state: string;
-    locked: boolean;
-    assignee: null;
-    assignees: any[];
-    milestone: null;
-    comments: number;
-    created_at: string;
-    updated_at: string;
-    closed_at: string | null;
-    author_association: string;
-    active_lock_reason: null;
-    body: string;
-    reactions: {
-        url: string;
-        total_count: number;
-        "+1": number;
-        "-1": number;
-        laugh: number;
-        hooray: number;
-        confused: number;
-        heart: number;
-        rocket: number;
-        eyes: number;
-    };
-    timeline_url: string;
-    performed_via_github_app: null;
-    state_reason: null;
-};
-type Result = {
-    date_created: string,
-    type: string,
-    id: number,
-    repo: string,
-    title: string,
-    user: string,
-    url: string
-};
+import {
+  GitHubIssue,
+  GitHubIssueSearchResult,
+  GitHubIssueResult,
+  AppResult
+} from './model.js';
 
 async function loadRepos(): Promise<string[]> {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const filePath = join(__dirname, '../repos.json');
-    const data = await readFile(filePath, 'utf8');
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, '../repos.json');
+  const data = await readFile(filePath, 'utf8');
 
-    const repos = JSON.parse(data);
-    console.log(repos);
-    return repos;
+  const repos = JSON.parse(data);
+  console.log(repos);
+  return repos;
 }
-function convertIssueToResult(issue: Issue): Result {
-    return {
-        date_created: issue.created_at,
-        type: issue.state,
-        id: issue.id,
-        repo: issue.repository_url,
-        title: issue.title,
-        user: issue.user.login,
-        url: issue.url
-    };
-}
-async function fetchData(url: string, repo: string): Promise<Issue[]> {
+async function loadUsers(): Promise<string[]> {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, '../users.json');
+  const data = await readFile(filePath, 'utf8');
 
-    const response = await fetch(url);
-
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    // @ts-ignore
-    const data: any = await response.json();
-
-    if (data.length === 0) {
-        return [];
-    }
-
-    return data;
-
+  const users = JSON.parse(data);
+  console.log(users);
+  return users;
 }
 
-async function processRepos(beginDate: string, endDate: string): Promise<Result[]> {
+function convertIssueToResult(issue: GitHubIssue): AppResult {
+  return {
+    date_created: issue.created_at,
+    type: issue.state,
+    id: issue.id,
+    repo: issue.repository_url,
+    title: issue.title,
+    user: issue.user.login,
+    url: issue.url
+  };
+}
+async function fetchRepoData(url: string): Promise<GitHubIssue[]> {
+  const response = await fetch(url);
 
-    let results: Result[] = [];
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
 
-    // Get data
-    for await (const repo of repos) {
+  const data: [] = (await response.json()) as [];
 
-        const url = `https://api.github.com/repos/${repo}/issues?created:${beginDate}..${endDate}`
-        const repoResult: Issue[] = await fetchData(url, repo);
+  if (data.length === 0) {
+    return [];
+  }
 
-        // Transform data
-        for (const item of repoResult) {
-            results.push(convertIssueToResult(item));
-        }
+  return data;
+}
+
+async function fetchIssues(url: string): Promise<GitHubIssue[]> {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = (await response.json()) as GitHubIssueSearchResult;
+
+  if (data.total_count === 0) {
+    return [];
+  }
+
+  return data.items as GitHubIssue[];
+}
+
+async function processRepos(
+  beginDate: string,
+  endDate: string,
+  repos: string[]
+): Promise<AppResult[]> {
+  const results: AppResult[] = [];
+
+  // Get data
+  for await (const repo of repos) {
+    const url = `https://api.github.com/repos/${repo}/issues?created:${beginDate}..${endDate}`;
+    const repoResult: GitHubIssue[] = await fetchRepoData(url);
+
+    // Transform data
+    for (const item of repoResult) {
+      results.push(convertIssueToResult(item));
     }
+  }
 
-    return results;
+  return results;
+}
+async function processUserIssues(
+  beginDate: string,
+  endDate: string,
+  user: string
+): Promise<AppResult[]> {
+  const results: AppResult[] = [];
+
+  const arrayUrls = [];
+  arrayUrls.push(
+    `https://api.github.com/search/issues?q=assignee:${user}+created:${beginDate}..${endDate}`
+  );
+  arrayUrls.push(
+    `https://api.github.com/search/issues?q=mentions:${user}+created:${beginDate}..${endDate}`
+  );
+
+  for await (const url of arrayUrls) {
+    const userIssues: GitHubIssue[] = await fetchIssues(url);
+
+    // Transform data
+    for (const item of userIssues) {
+      results.push(convertIssueToResult(item));
+    }
+  }
+
+  return results;
 }
 function getDates(beginDays: string, endDays: string) {
+  // Parse beginDays and endDays as integers
+  const beginDaysInt = parseInt(beginDays, 10);
+  const endDaysInt = parseInt(endDays, 10);
 
-    // Parse beginDays and endDays as integers
-    const beginDaysInt = parseInt(beginDays, 10);
-    const endDaysInt = parseInt(endDays, 10);
+  // Check if beginDays and endDays are valid integers
+  if (isNaN(beginDaysInt) || isNaN(endDaysInt)) {
+    throw new Error('beginDays and endDays must be valid integers');
+  }
 
-    // Check if beginDays and endDays are valid integers
-    if (isNaN(beginDaysInt) || isNaN(endDaysInt)) {
-        throw new Error('beginDays and endDays must be valid integers');
-    }
+  // Get the current date
+  const currentDate = new Date();
 
-    // Get the current date
-    let currentDate = new Date();
+  // Get the date of beginDays days ago
+  const beginDate = new Date();
+  beginDate.setDate(currentDate.getDate() - parseInt(beginDays));
 
-    // Get the date of beginDays days ago
-    const beginDate = new Date();
-    beginDate.setDate(currentDate.getDate() - parseInt(beginDays));
+  // Get the date of endDays days ago
+  const endDate = new Date();
+  endDate.setDate(currentDate.getDate() - parseInt(endDays));
 
-    // Get the date of endDays days ago
-    const endDate = new Date();
-    endDate.setDate(currentDate.getDate() - parseInt(endDays));
+  // Format the dates in YYYY-MM-DD format
+  const beginDateString = beginDate.toISOString().split('T')[0];
+  const endDateString = endDate.toISOString().split('T')[0];
 
-    // Format the dates in YYYY-MM-DD format
-    const beginDateString = beginDate.toISOString().split('T')[0];
-    const endDateString = endDate.toISOString().split('T')[0];
+  // Output as markdown with links to URLs
+  const formatBeginDate = `${beginDate.toLocaleString('default', {
+    month: 'long'
+  })} ${beginDate.getDate()}, ${beginDate.getFullYear()}`;
+  const formatEndDate = `${endDate.toLocaleString('default', {
+    month: 'long'
+  })} ${endDate.getDate()}, ${endDate.getFullYear()}`;
 
-    // Output as markdown with links to URLs
-    const formatBeginDate = `${beginDate.toLocaleString('default', { month: 'long' })} ${beginDate.getDate()}, ${beginDate.getFullYear()}`;
-    const formatEndDate = `${endDate.toLocaleString('default', { month: 'long' })} ${endDate.getDate()}, ${endDate.getFullYear()}`;
-
-    return { beginDate, endDate, formatBeginDate, formatEndDate, beginDateString, endDateString };
+  return {
+    beginDate,
+    endDate,
+    formatBeginDate,
+    formatEndDate,
+    beginDateString,
+    endDateString
+  };
 }
+function printData(
+  beginDate: string,
+  endDate: string,
+  reportName: string,
+  data: AppResult[]
+) {
+  console.log(`# ${reportName} from ${beginDate} to ${endDate}`);
+  console.log(`|Id|Type|Repo|User|Title|Date|`);
+  console.log(`|--|--|--|--|--|--|`);
 
-
-// Load list of repos
-const repos: string[] = await loadRepos();
-
-// Get the dates
-const previousDaysBegin = process.env.PREVIOUS_DAYS_BEGIN as string || "8";
-const previousDaysEnd = process.env.PREVIOUS_DAYS_End as string || "1";
-const dates = getDates(previousDaysBegin, previousDaysEnd);
-
-// Get data and transform it
-const issuesAndPrs = await processRepos(dates.beginDateString, dates.endDateString);
-
-// Present data
-console.log(`# GitHub issues and prs from ${dates.formatBeginDate} to ${dates.formatEndDate}`);
-console.log(`|Id|Type|Repo|User|Title|Date|`);
-console.log(`|--|--|--|--|--|--|`);
-
-issuesAndPrs.forEach((item: Result) => {
-
+  data.forEach((item: AppResult) => {
     // split url into its parts
     const urlParts = item.url.split('/');
     const owner = urlParts[3];
@@ -199,5 +179,52 @@ issuesAndPrs.forEach((item: Result) => {
     // remove time from date
     item.date_created = item.date_created.split(',')[0];
 
-    console.log(`|${item.id}|${item.type}|${owner}/${repo}|${user}| [${item.title}](${item.url})|${item.date_created}|`)
-});
+    console.log(
+      `|${item.id}|${item.type}|${owner}/${repo}|${user}| [${item.title}](${item.url})|${item.date_created}|`
+    );
+  });
+}
+
+// Load list of repos
+const repos: string[] = await loadRepos();
+
+// Load list of users
+const users: string[] = await loadUsers();
+
+// Get the dates
+const previousDaysBegin = (process.env.PREVIOUS_DAYS_BEGIN as string) || '8';
+const previousDaysEnd = (process.env.PREVIOUS_DAYS_END as string) || '1';
+const dates = getDates(previousDaysBegin, previousDaysEnd);
+
+// Get data and transform it
+const issuesAndPrs = await processRepos(
+  dates.beginDateString,
+  dates.endDateString,
+  repos
+);
+printData(
+  dates.formatBeginDate,
+  dates.formatEndDate,
+  'Repos - GitHub issues and prs',
+  issuesAndPrs
+);
+
+// Assigned to users
+const assignedIssuesAndPrs: AppResult[] = [];
+
+// Get data for users
+for await (const user of users) {
+  const userIssues = await processUserIssues(
+    dates.beginDateString,
+    dates.endDateString,
+    user
+  );
+  assignedIssuesAndPrs.push(...userIssues);
+}
+
+printData(
+  dates.formatBeginDate,
+  dates.formatEndDate,
+  'Assigned - GitHub issues and prs',
+  assignedIssuesAndPrs
+);
