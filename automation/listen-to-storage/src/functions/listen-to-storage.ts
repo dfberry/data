5d626364cbe3cb11a7d67f2a905681cd8863fd47
Dina Berry <dinaberry@outlook.com>
2024-05-12 08:51:07 -0700

    azure function storage to db

---
+++ automation/listen-to-storage/src/functions/listen-to-storage.ts
@@ -0,0 +1,36 @@
+import { app, InvocationContext, output } from "@azure/functions";
+import { v4 as uuidv4 } from 'uuid';
+
+const sendToCosmosDb = output.cosmosDB({
+    databaseName: 'github_history',
+    containerName: 'data_processing',
+    connection: 'AZURE_COSMOSDB_CONNECTION_STRING',
+});
+
+export async function listenToStorage(blob: Buffer, context: InvocationContext): Promise<void> {
+    context.log(`Storage blob function processed blob "${context.triggerMetadata.name}" with size ${blob.length} bytes`);
+
+    const docForDatabase = {
+        // create a random ID
+        id: uuidv4(),
+        url: context.triggerMetadata.uri,
+        name: context.triggerMetadata.name,
+        properties: context.triggerMetadata.properties,
+        path: context.triggerMetadata.path,
+        length: blob.length,
+        dateUploaded: new Date().toISOString(),
+    }
+
+    context.log(`Document to crete for CosmosDB: ${JSON.stringify(docForDatabase)}`)
+    context.extraOutputs.set(sendToCosmosDb, docForDatabase);
+
+    context.log('Document created for CosmosDB');
+
+}
+
+app.storageBlob('listen-to-storage', {
+    path: 'my-history/{name}',
+    connection: 'AZURE_STORAGE_CONNECTION_STRING',
+    extraOutputs: [sendToCosmosDb],
+    handler: listenToStorage
+});
