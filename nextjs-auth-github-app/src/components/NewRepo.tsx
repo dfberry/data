c7c9a161de89f86bd461120e67cf1f49b00995d4
Dina Berry <dinaberry@outlook.com>
2024-07-09 07:22:56 -0700

    styling

--- nextjs-auth-github-app/src/components/NewRepo.tsx
+++ nextjs-auth-github-app/src/components/NewRepo.tsx
@@ -2,10 +2,10 @@ import { createNewRepo } from "@/actions/repo";
 const NewRepo = async () => {
     return (
         <form action={createNewRepo}>
-            <div className="flex flex-col">
-                <label>Repo Name</label>
-                <input type="text" name="repo" className="mt-2 border border-gray-200"/>
-                <button type="submit">create</button>
+            <div className="flex items-center">
+                <label className="mr-4">Repo Name</label>
+                <input type="text" name="repo" className="flex-1 mt-2 border border-gray-200" />
+                <button className="ml-4 bg-green-500 text-white px-4 py-1 rounded hover:bg-green-700 transition-colors" type="submit">Create</button>
             </div>
         </form>
     );
--- nextjs-auth-github-app/src/components/Repo.tsx
+++ nextjs-auth-github-app/src/components/Repo.tsx
@@ -4,8 +4,8 @@ import { deleteRepo } from '@/actions/repo'
 const Repo = ({ repo }:any) => {
   const [isPending, startTransition] = useTransition()
   return (
-    <div className="px-8 py-2 border border-black/25 flex justify-between items-center">
-      <span>{repo.url}</span>
+    <div className="flex items-center justify-between">
+      <span className="flex-1">{repo.url}</span>
       <button
         className="bg-red-500 text-white px-4 py-1 rounded hover:bg-red-700 transition-colors"
         disabled={isPending}
