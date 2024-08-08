beab91074f63768623fda03e3a174a19bd438c53
Dina Berry (personal) <dfberry@users.noreply.github.com>
2024-08-07 19:05:41 -0700

    Diberry/deploy 9 (#12)
    * Add role to sp
    
    * fix script
    
    * sp cred

---
+++ scripts/add-role-to-sp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Variables
+SERVICE_PRINCIPAL_ID="your-service-principal-id"
+
+# With Azure CLI, add role to service principal
+CONTRIBUTOR_ROLE="b24988ac-6180-42a0-ab88-20f7382dd24c"
+SUBSCRIPTION_ID="your-subscription-id"
+RESOURCE_GROUP_NAME="your-resource-group-name"
+SCOPE="/subscriptions/your-subscription-id/resourceGroups/your-resource-group-name"
+
+# Add role to service principal
+az role assignment create --assignee "$SERVICE_PRINCIPAL_ID" --role "$CONTRIBUTOR_ROLE" --scope "$SCOPE"
